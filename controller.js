9baff89814693923ac2d727928bc0fe9bcfcdbcd
manishaac2002 <manishaac2002@gmail.com>
2024-01-13 11:40:21 +0530

    feat: rework the databse.

--- controller.js
+++ controller.js
@@ -1,26 +1,14 @@
-const { Client, Users } = require("./db");
+const {  Users_Demo, clients } = require("./db");
 
 
 const getClientDetailsController = (request, response) => {
-    response.send('Getting client details')
+  response.send('Getting client details')
 }
 const createNewClientDetailsController = async (request, response) => {
-    try {
-        // Insert data into Table1
-        const clientTable = await Client.create(data1);
+  
+}
 
-        // Insert data into Table2
-        const usersTable = await Users.create(data2);
-
-        return res.status(200).json({ message: 'Data inserted successfully' });
-      } catch (error) {
-        // Handle errors
-        console.error(error);
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-    }
-
-module.exports = { 
-    getClientDetailsController ,
-    createNewClientDetailsController 
+module.exports = {
+  getClientDetailsController,
+  createNewClientDetailsController
 } 
