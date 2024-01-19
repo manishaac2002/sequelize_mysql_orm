19eae7965600b20e444a9b157cff18f4a4e7a7cc
manishaac2002 <manishaac2002@gmail.com>
2024-01-19 21:25:45 +0530

    feat:sequelize dual table worked .

--- controller.js
+++ controller.js
@@ -1,18 +1,18 @@
 import dbTable from './models/db.js'
 const createNewUserDetailsController = async(request, response) => {
-  const {user_name} =request.body
+  const {user_name,user_email,user_id} =request.body
   const newUser =await dbTable.User.create({
     user_name ,
   })
-  response.json(newUser)
+  const newUserDetail =await dbTable.UserDetail.create({
+   user_email,user_id
+  })
+  response.json(newUser && newUserDetail )
   console.log(request.body);
 }
 
 const createNewUserDetailsController1 = async(request, response) => {
-  const {user_email,user_id} =request.body
-  const newUser =await dbTable.UserDetail.create({
-   user_email,user_id
-  })
+
   response.json(newUser)
 }
 
